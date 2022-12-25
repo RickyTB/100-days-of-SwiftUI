c9d763156059a22549bb3f0db32c3c9c3d406a71
Ricardo Baquero <wola_ricky@hotmail.com>
2022-12-25 16:12:39 -0500

    Project 2 finished

---
+++ 2. GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ricardo Baquero on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
