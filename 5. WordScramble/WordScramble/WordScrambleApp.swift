560a533144bd6ab35aff9290fdae36b5831c495d
Ricardo Baquero <wola_ricky@hotmail.com>
2022-12-25 21:09:10 -0500

    Project 5 finished

---
+++ 5. WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ricardo Baquero on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
