0cf2e35bcaf43d348165375936f0435b4ae92dab
Ricardo Baquero <wola_ricky@hotmail.com>
2022-12-26 23:49:07 -0500

    Project 8 finished

---
+++ 8. Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Ricardo Baquero on 26/12/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
