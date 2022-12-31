1c4f96ea0f27e921cae27c8674bfd6b5f65ea3d6
Ricardo Baquero <wola_ricky@hotmail.com>
2022-12-30 22:55:13 -0500

    Project 10 added

---
+++ 10. CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ricardo Baquero on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
